fce19f604c683981d4d254e6189cf1b1a12f41dc
Amina Masera <aminakavele@gmail.com>
2026-04-28 00:51:10 +0400

    Not yet ready for submission

--- index.js
+++ index.js
@@ -1,54 +1,43 @@
 function calculateTax(amount) {
-    let tax = 0.10 * amount;
-    return tax;
+    let tax = 0.10 * amount
+    return tax
 }
 
-function convertToUpperCase(text) {
-    let string = text.toUpperCase();
-    return string;
+function convertToUpperCase(text){
+   let string =  text.toUpperCase()
+   return string
 }
 
-function findMaximum(num1,num2) {
-    if (num1 > num2) {
-        return num1;
+function findMaximum(num1,num2){
+    if(num1 > num2){
+        return num1
     }
-    else {
-        return num2;
+    else{
+        return num2
     }
 }
 
 function isPalindrome(word){
-    let pali = "";
-    for (let i = word.length - 1; i >= 0; i--) {
-        pali += word[i]
+    let pali = ""
+    for(let w = word.length-1; w >= 0; w--){
+        pali += word[1]
     }
-    if (pali == word) {
+    if(pali == word){
         return true
     }
     else {
-        return false;
+        return false
     }
-}
-let pali1 = "racecar"
-let pali2 = "deed"
-let pali3 = "flop"
 
-console.log("Palindrome1: ", isPalindrome(pali1));
-console.log("Palindrome2: ", isPalindrome(pali2));
-console.log("Palindrome3: ", isPalindrome(pali3));
-
-
-function calculateDiscountedPrice(originalPrice, discountPercentage){
-    let discountedPrice = originalPrice - (originalPrice * discountPercentage/100) 
-    return discountedPrice;
 }
 
-console.log ("Discounted Price:", calculateDiscountedPrice(100, 20));
-console.log ("This is the maximum:",findMaximum(5,10))
-console.log ("Tax Amount:", calculateTax(60));
-console.log ("UpperCase Text:", convertToUpperCase("yeahiletgo"))
+function calculateDiscountedPrice(originalPrice,discountPercentage){
+    let discountedPrice = originalPrice - (originalPrice * (discountPercentage/100))
+    return discountedPrice
+}
 
-
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
-
+console.log("Tax Calculation:",calculateTax(100))
+console.log("Uppercase conversion:",convertToUpperCase("flop"))
+console.log("Maximum number:",findMaximum(9,3))
+console.log("Palindrome boolean:",isPalindrome("rotator"))
+console.log("The dicounted price:",calculateDiscountedPrice(100,20))
